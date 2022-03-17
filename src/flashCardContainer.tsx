d58df6212923b6fcaf7f2ab4dc8bd45c1899f2a6
Baptiste Verkindere <bverkindere@gmail.com>
2022-03-17 17:30:45 +0100

    adding multiple choice question

--- src/flashCardContainer.tsx
+++ src/flashCardContainer.tsx
@@ -4,10 +4,27 @@ import { MentalCard } from "./mentalCard";
 import { MultipleChoice } from "./multipleChoiceCard";
 
 export const FlashCardContainer = (): JSX.Element => {
-  const [question, setQuestion] = React.useState("")
+  const [gameType, setGameType] = React.useState("MentalCard")
+
+function pickGame (){
+
+}
+
   return (
     <>
-    <MentalCard />
+    <select>
+        <option value="Mental_card" onClick={()=> setGameType("MentalCard")}>
+              Mental card
+            </option>
+            <option value="Input_card">
+              Input card
+            </option>
+            <option value="Multiple_choice_card">
+              Multiple choice card
+          </option>
+    </select>
+
+     <MentalCard />
     <InputCard />
     <MultipleChoice />
     </>
--- src/multipleChoiceCard.tsx
+++ src/multipleChoiceCard.tsx
@@ -17,13 +17,13 @@ const mentalQuestions: Questions[] = [
   },
   {
     question: "My second question",
-    answer2: "Answer to my second question",
+    answer2: "Answer to my first question",
     answer: "Answer to my second question",
     answer3: "Answer to my third question"
   },
   {
     question: "My third question",
-    answer2: "Answer to my third question",
+    answer2: "Answer to my first question",
     answer3: "Answer to my second question",
     answer: "Answer to my third question"
   },
@@ -31,19 +31,58 @@ const mentalQuestions: Questions[] = [
 
 export const MultipleChoice = (): JSX.Element => {
   const [question, setQuestion] = React.useState("My first question")
-  const [answer, setAnswer] = React.useState("")
+  const [answer, setAnswer] = React.useState("Answer to my first question")
+  const [answer2, setAnswer2] = React.useState("Answer to my second question")
+  const [answer3, setAnswer3] = React.useState("Answer to my third question")
+  const [playerAnswer, setPlayerAnswer] = React.useState("")
+  const [answerFlag, setAnswerFlag] = React.useState(false)
+  const [displayQuestion, setDisplayQuestion] = React.useState(true)
+
   const [flag, setFlag] = React.useState(true)
 
   function loadQuestion (): void {
+    setDisplayQuestion(true)
     const generateRandom: number = Math.round(Math.random()*(mentalQuestions.length-1));
     setQuestion(mentalQuestions[generateRandom].question)
     setAnswer(mentalQuestions[generateRandom].answer)
+    setAnswer2(mentalQuestions[generateRandom].answer2)
+    setAnswer3(mentalQuestions[generateRandom].answer3)
     setFlag(!flag)
+    setAnswerFlag(false)
+  }
+
+  function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    console.log("test", e);
+    console.log("You clicked submit.");
+  }
+
+  function loadResponse(){
+    setDisplayQuestion(!displayQuestion)
+    if(playerAnswer === answer){
+      setAnswerFlag(true)
+    }
   }
 
   return (
     <>
-    <div onClick={loadQuestion}>{flag ? question : answer}</div>
+    {displayQuestion ?
+    <>
+      <p> {question} </p>
+      <form onSubmit={handleSubmit}>
+      <ul>
+          <input type="checkbox" onClick={()=>setPlayerAnswer(answer)}/>{answer}<br></br>
+          <input type="checkbox" onClick={()=>setPlayerAnswer(answer2)}/>{answer2}<br></br>
+          <input type="checkbox" onClick={()=>setPlayerAnswer(answer3)}/>{answer3}<br></br>
+      </ul>
+      <button type="submit" className="btn btn-primary" onClick={loadResponse}>
+        Submit
+      </button>
+    </form>
     </>
+    : <div onClick={loadQuestion}>{answerFlag ? <p>✅ Good answer</p> : <p>❌ Wrong answer</p> }</div>}
+
+    </>
+
   )
 }
